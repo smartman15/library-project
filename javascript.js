8bb60acadf3d91e0ed43dbd9b98032b84bcf97d0
khayri <khayriramzy@gmail.com>
2026-07-07 12:23:53 -0400

    Create function to add book to myLIbrary array

--- javascript.js
+++ javascript.js
@@ -4,9 +4,15 @@ function Book(author, title, pages, read){
     if(!new.target){
         throw Error("you gotta use the 'new' keyword bro")
     }
-    
+
     this.author = author;
     this.title = title;
     this.pages = pages;
     this.read = read;
+}
+
+
+function addBookToLibrary(author, title, pages, read){
+    const book = new Book(author, title, pages, read);
+    myLibrary.push(book);
 }
